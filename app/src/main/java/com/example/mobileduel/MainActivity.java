98e236710cef4875d8e3a7469ea608e900b3215c
Dmitry Ovchinikov <thevitamine0@gmail.com>
2020-08-14 20:35:26 +0300

    Added background, a Player class, tinkered with action button animations.

--- app/src/main/java/com/example/mobileduel/MainActivity.java
+++ app/src/main/java/com/example/mobileduel/MainActivity.java
@@ -25,18 +25,62 @@ public class MainActivity extends AppCompatActivity {
     private ProgressBar main_BAR_leftPlayer;
     private ProgressBar main_BAR_rightPlayer;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Player left_player = new Player(50, true);
+        Player right_player = new Player(50,true);
+
         findViews();
         glideIMGs();
+        initializeProgressBars(left_player, right_player);
+        playGame();
+    }
+
+    private void initializeProgressBars(Player left_player, Player right_player) {
+        main_BAR_leftPlayer.setMax(left_player.getHealth());
+        main_BAR_leftPlayer.setProgress(left_player.getHealth());
+
+        main_BAR_rightPlayer.setMax(right_player.getHealth());
+        main_BAR_rightPlayer.setProgress(right_player.getHealth());
+    }
+
+    private void playGame() {
+        activateButtons("left");
+        deactivateButtons("right");
+    }
+
+    private void activateButtons(String side) {
+        if (side.equals("left")) {
+            main_BTN_left1.setEnabled(true);
+            main_BTN_left2.setEnabled(true);
+            main_BTN_left3.setEnabled(true);
+        } else {
+            main_BTN_right1.setEnabled(true);
+            main_BTN_right2.setEnabled(true);
+            main_BTN_right3.setEnabled(true);
+        }
+    }
+
+    private void deactivateButtons(String side) {
+        if (side.equals("left")) {
+            main_BTN_left1.setEnabled(false);
+            main_BTN_left2.setEnabled(false);
+            main_BTN_left3.setEnabled(false);
+        } else {
+            main_BTN_right1.setEnabled(false);
+            main_BTN_right2.setEnabled(false);
+            main_BTN_right3.setEnabled(false);
+        }
     }
 
     private void glideIMGs() {
         Glide.with(this).load(R.drawable.left_player).into(main_IMG_leftPlayer);
         Glide.with(this).load(R.drawable.right_player).into(main_IMG_rightPlayer);
+        Glide.with(this).load(R.drawable.main_background).into(main_IMG_background);
 
     }
 
@@ -50,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
         main_IMG_leftPlayer = findViewById(R.id.main_IMG_leftPlayer);
         main_IMG_rightPlayer = findViewById(R.id.main_IMG_rightPlayer);
+        main_IMG_background = findViewById(R.id.main_IMG_background);
 
         main_BAR_leftPlayer = findViewById(R.id.main_BAR_leftPlayer);
         main_BAR_rightPlayer = findViewById(R.id.main_BAR_rightPlayer);
---
+++ app/src/main/java/com/example/mobileduel/Player.java
@@ -0,0 +1,50 @@
+package com.example.mobileduel;
+
+public class Player {
+
+    //true state = alive, false state = dead
+
+    private int health;
+    private boolean state;
+
+    public Player() {
+    }
+
+    public Player(int health, boolean state) {
+        this.health = health;
+        this.state = state;
+    }
+
+    public void action(int num) {
+        if (num == 0) { //Light Attack
+            this.health -= 5;
+        } else if (num == 1) { //Heavy Attack
+            this.health -= 10;
+        } else {
+            this.health += 15; //Heal
+        }
+    }
+
+    //If health is below 0, you lost.
+    public void checkState() {
+        if (this.health <= 0) {
+            this.setState(false);
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+}
