22fd2bbce84844d3109d0295076639d0eab212da
Dmitry Ovchinikov <thevitamine0@gmail.com>
2020-08-17 00:07:16 +0300

    Game sharded into three activities so far: main / automaticGame / manualGame. First commit of the second exercise.

---
+++ app/src/main/java/com/example/mobileduel/activity_main.java
@@ -0,0 +1,92 @@
+package com.example.mobileduel;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+import com.bumptech.glide.Glide;
+
+//TODO: Button that sends to the records activity
+//TODO: activity after game that tells how many actions were used and who won
+//TODO: top 10 record activity with a MAP
+//TODO: sounds / music
+
+public class activity_main extends AppCompatActivity {
+
+    private Button main_BTN_play;
+
+    private SwitchCompat main_SWT_mode;
+
+    private ImageView main_IMG_background;
+
+    private TextView main_LBL_auto;
+    private TextView main_LBL_manual;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViews();
+        glideIMGs();
+
+        main_BTN_play.setOnClickListener(onClickListener);
+
+        main_SWT_mode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    main_LBL_manual.setTypeface(null, Typeface.BOLD);
+                    main_LBL_auto.setTypeface(null, Typeface.NORMAL);
+                } else {
+                    main_LBL_auto.setTypeface(null, Typeface.BOLD);
+                    main_LBL_manual.setTypeface(null, Typeface.NORMAL);
+                }
+            }
+        });
+
+
+
+    }
+
+    private void glideIMGs() {
+        Glide.with(this).load(R.drawable.main_background).into(main_IMG_background);
+    }
+
+    private void findViews() {
+        main_BTN_play = findViewById(R.id.main_BTN_play);
+        main_SWT_mode = findViewById(R.id.main_SWT_mode);
+        main_IMG_background = findViewById(R.id.main_IMG_background);
+        main_LBL_auto = findViewById(R.id.main_LBL_auto);
+        main_LBL_manual = findViewById(R.id.main_LBL_manual);
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (main_SWT_mode.isChecked()) {
+                playManual();
+            } else {
+                playAutomatic();
+            }
+        }
+    };
+
+    private void playAutomatic() {
+        //TODO: Automatic mode
+    }
+
+    private void playManual() {
+        Intent intent = new Intent(this, activity_manualGame.class);
+        startActivity(intent);
+    }
+}
