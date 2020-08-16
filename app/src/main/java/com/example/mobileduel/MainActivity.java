fb46b30fe0c83a1abb30a9ea22f70d758212770c
Dmitry Ovchinikov <thevitamine0@gmail.com>
2020-08-16 16:16:48 +0300

    Visibility and comments, minor changes.

--- app/src/main/java/com/example/mobileduel/MainActivity.java
+++ app/src/main/java/com/example/mobileduel/MainActivity.java
@@ -13,16 +13,14 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
-import static android.graphics.BlendMode.COLOR;
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button main_BTN_left1;
-    private Button main_BTN_left2;
-    private Button main_BTN_left3;
-    private Button main_BTN_right1;
-    private Button main_BTN_right2;
-    private Button main_BTN_right3;
+    private Button main_BTN_leftlight;
+    private Button main_BTN_leftheavy;
+    private Button main_BTN_leftheal;
+    private Button main_BTN_rightlight;
+    private Button main_BTN_rightheavy;
+    private Button main_BTN_rightheal;
 
     private ImageView main_IMG_leftPlayer;
     private ImageView main_IMG_rightPlayer;
@@ -52,15 +50,6 @@ public class MainActivity extends AppCompatActivity {
         deactivateButtons("right");
     }
 
-    private void setButtonListeners() {
-        main_BTN_left1.setOnClickListener(clickListener);
-        main_BTN_left2.setOnClickListener(clickListener);
-        main_BTN_left3.setOnClickListener(clickListener);
-        main_BTN_right1.setOnClickListener(clickListener);
-        main_BTN_right2.setOnClickListener(clickListener);
-        main_BTN_right3.setOnClickListener(clickListener);
-    }
-
     private void initializeProgressBars() {
         main_BAR_leftPlayer.setMax(left_player.getHealth());
         main_BAR_leftPlayer.setProgress(left_player.getHealth());
@@ -72,79 +61,81 @@ public class MainActivity extends AppCompatActivity {
         main_BAR_rightPlayer.getProgressDrawable().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
     }
 
-
-    private void activateButtons(String side) {
-        if (side.equals("left")) {
-            main_BTN_left1.setEnabled(true);
-            main_BTN_left2.setEnabled(true);
-            main_BTN_left3.setEnabled(true);
-        } else {
-            main_BTN_right1.setEnabled(true);
-            main_BTN_right2.setEnabled(true);
-            main_BTN_right3.setEnabled(true);
-        }
-    }
-
+    //Decided to use the switch onClickListener method regardless of the fact you have said its bad in one of the lectures, so I won't have to duplicate a lot of code.
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.main_BTN_left1:
-                    right_player.action(0);
-                    activateButtons("right");
-                    deactivateButtons("left");
+                case R.id.main_BTN_leftlight:
+                    playTurn(right_player, 0, "right", "left");
                     break;
-                case R.id.main_BTN_left2:
-                    right_player.action(1);
-                    activateButtons("right");
-                    deactivateButtons("left");
+                case R.id.main_BTN_leftheavy:
+                    playTurn(right_player, 1, "right", "left");
                     break;
-                case R.id.main_BTN_left3:
-                    left_player.action(2);
-                    activateButtons("right");
-                    deactivateButtons("left");
+                case R.id.main_BTN_leftheal:
+                    playTurn(left_player, 2, "right", "left");
                     break;
-                case R.id.main_BTN_right1:
-                    left_player.action(0);
-                    activateButtons("left");
-                    deactivateButtons("right");
+                case R.id.main_BTN_rightlight:
+                    playTurn(left_player, 0, "left", "right");
                     break;
-                case R.id.main_BTN_right2:
-                    left_player.action(1);
-                    activateButtons("left");
-                    deactivateButtons("right");
+                case R.id.main_BTN_rightheavy:
+                    playTurn(left_player, 1, "left", "right");
                     break;
-                case R.id.main_BTN_right3:
-                    right_player.action(2);
-                    activateButtons("left");
-                    deactivateButtons("right");
+                case R.id.main_BTN_rightheal:
+                    playTurn(right_player, 2, "left", "right");
                     break;
                 default:
                     break;
             }
-            updateProgressBars();
-            boolean left_state = left_player.checkState();
-            boolean right_state = right_player.checkState();
-            checkWin(left_state, right_state);
         }
     };
 
+    //Play a single turn.
+    private void playTurn(Player player, int x, String activated_side, String deactivated_side) {
+        // x:
+        // 0 - Light attack
+        // 1 - Heavy attack
+        // 2 - Heal
+
+        //play an action, activate and deactivate the relevant buttons
+        player.action(x);
+        activateButtons(activated_side);
+        deactivateButtons(deactivated_side);
+
+        updateProgressBars();
+
+        checkWin();
+    }
+
+    //Update the progress bars.
     private void updateProgressBars() {
         main_BAR_leftPlayer.setProgress(left_player.getHealth());
         main_BAR_rightPlayer.setProgress(right_player.getHealth());
+
+        updateProgressBarColors();
+    }
+
+    //Change the color of the progress bars if they are below 15 to RED, otherwise to BLUE.
+    private void updateProgressBarColors() {
         if (main_BAR_leftPlayer.getProgress() <= 15) {
             main_BAR_leftPlayer.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        } else {
+            main_BAR_leftPlayer.getProgressDrawable().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
         }
+
         if (main_BAR_rightPlayer.getProgress() <= 15) {
             main_BAR_rightPlayer.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        } else {
+            main_BAR_rightPlayer.getProgressDrawable().setColorFilter(Color.BLUE, PorterDuff.Mode.SRC_IN);
         }
     }
 
-    private void checkWin(boolean left_state, boolean right_state) {
-        if (left_state == false) {
+    //Check if a player won the game.
+    private void checkWin() {
+        if (!left_player.checkState()) {
             Toast.makeText(this, "Right player won!", Toast.LENGTH_SHORT).show();
             resetGame();
-        } else if (right_state == false) {
+        } else if (!right_player.checkState()) {
             Toast.makeText(this, "Left player won!", Toast.LENGTH_SHORT).show();
             resetGame();
         }
@@ -159,17 +150,37 @@ public class MainActivity extends AppCompatActivity {
         initializeProgressBars();
     }
 
-
     private void deactivateButtons(String side) {
         if (side.equals("left")) {
-            main_BTN_left1.setEnabled(false);
-            main_BTN_left2.setEnabled(false);
-            main_BTN_left3.setEnabled(false);
+            main_BTN_leftlight.setEnabled(false);
+            main_BTN_leftheavy.setEnabled(false);
+            main_BTN_leftheal.setEnabled(false);
         } else {
-            main_BTN_right1.setEnabled(false);
-            main_BTN_right2.setEnabled(false);
-            main_BTN_right3.setEnabled(false);
+            main_BTN_rightlight.setEnabled(false);
+            main_BTN_rightheavy.setEnabled(false);
+            main_BTN_rightheal.setEnabled(false);
         }
+    }
+
+    private void activateButtons(String side) {
+        if (side.equals("left")) {
+            main_BTN_leftlight.setEnabled(true);
+            main_BTN_leftheavy.setEnabled(true);
+            main_BTN_leftheal.setEnabled(true);
+        } else {
+            main_BTN_rightlight.setEnabled(true);
+            main_BTN_rightheavy.setEnabled(true);
+            main_BTN_rightheal.setEnabled(true);
+        }
+    }
+
+    private void setButtonListeners() {
+        main_BTN_leftlight.setOnClickListener(clickListener);
+        main_BTN_leftheavy.setOnClickListener(clickListener);
+        main_BTN_leftheal.setOnClickListener(clickListener);
+        main_BTN_rightlight.setOnClickListener(clickListener);
+        main_BTN_rightheavy.setOnClickListener(clickListener);
+        main_BTN_rightheal.setOnClickListener(clickListener);
     }
 
     private void glideIMGs() {
@@ -179,12 +190,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void findViews() {
-        main_BTN_left1 = findViewById(R.id.main_BTN_left1);
-        main_BTN_left2 = findViewById(R.id.main_BTN_left2);
-        main_BTN_left3 = findViewById(R.id.main_BTN_left3);
-        main_BTN_right1 = findViewById(R.id.main_BTN_right1);
-        main_BTN_right2 = findViewById(R.id.main_BTN_right2);
-        main_BTN_right3 = findViewById(R.id.main_BTN_right3);
+        main_BTN_leftlight = findViewById(R.id.main_BTN_leftlight);
+        main_BTN_leftheavy = findViewById(R.id.main_BTN_leftheavy);
+        main_BTN_leftheal = findViewById(R.id.main_BTN_leftheal);
+        main_BTN_rightlight = findViewById(R.id.main_BTN_rightlight);
+        main_BTN_rightheavy = findViewById(R.id.main_BTN_rightheavy);
+        main_BTN_rightheal = findViewById(R.id.main_BTN_rightheal);
 
         main_IMG_leftPlayer = findViewById(R.id.main_IMG_leftPlayer);
         main_IMG_rightPlayer = findViewById(R.id.main_IMG_rightPlayer);
